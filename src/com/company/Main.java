02f8a8618cb384ea352023028ac20d15d1f4fe00
Andrius <andriuscere@gmail.com>
2018-01-30 19:58:42 +0200

    Uzduotis11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ivedimas();
+        System.out.println("Suma: " + skaiciuotuvas.suma(a,b));
+        System.out.println("Suma: " + skaiciuotuvas.suma((float) a, (float) b));
+        System.out.println("Skirtumas: " + skaiciuotuvas.skirtumas(a,b));
+        System.out.println("Skirtumas: " + skaiciuotuvas.skirtumas((float)a, (float)b));
+        System.out.println("Sandauga: " + skaiciuotuvas.sandauga(a,b));
+        System.out.println("Sandauga: " + skaiciuotuvas.sandauga((float)a,(float)b));
+    }
+    static int a;
+    static int b;
+
+    public static void ivedimas(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite du skaicius");
+        a = sc.nextInt();
+        b = sc.nextInt();
+
     }
 }
